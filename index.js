e43762beef1c8390096e34937dcccf715cd76a8a
Paul <persimmon212@gmail.com>
2020-11-14 13:37:10 +0900

    adding the basic outline of the page

---
+++ index.js
@@ -0,0 +1,8 @@
+//variables
+const urlBase = 'https://api.punkapi.com/v2/beers';
+
+//create async function called 'getBeers' to fetch our beer data from the urlBase.
+//render each beer name inside the div w/class of beers that currently exists in the Html file.
+const getBeers = async () => {
+    
+}
